c3a640e5e4d9d17f1c3edeeef704f5eb28982a93
Muedgar <edgarmutangana@gmail.com>
2021-12-06 08:50:51 +0200

    Add files via upload

---
+++ UI/LandingPage/devops.js
@@ -0,0 +1,62 @@
+/*
+the ui to view
+
+<article class="article-container">
+                <a class="article-link" href="#">
+                    <img class="article-img" src="./linkComponent/img1.jpeg" alt="" srcset="">
+                    <h4 class="article-author">Authored by Mutangana Edgar</h4>
+                    <p class="article-summary">The Surface Pro shares a lot of its design traits with its lower cost counterpart, However, the resolution on the Surface Pro is higher and is 1920x1080, and the screen</p>
+                </a>
+ </article>
+*/
+
+let devOps = {
+    article1: {
+        imagePreviewLink: "",
+        image1Link: "",
+        image2Link: "",
+        authorName: "",
+        articlePreview: "",
+        articleContent: ""
+    },
+    article2: {
+        imagePreviewLink: "",
+        image1Link: "",
+        image2Link: "",
+        authorName: "",
+        articlePreview: "",
+        articleContent: ""
+    },
+    article3: {
+        imagePreviewLink: "",
+        image1Link: "",
+        image2Link: "",
+        authorName: "",
+        articlePreview: "",
+        articleContent: ""
+    },
+    article4: {
+        imagePreviewLink: "",
+        image1Link: "",
+        image2Link: "",
+        authorName: "",
+        articlePreview: "",
+        articleContent: ""
+    },
+    article5: {
+        imagePreviewLink: "",
+        image1Link: "",
+        image2Link: "",
+        authorName: "",
+        articlePreview: "",
+        articleContent: ""
+    },
+    article6: {
+        imagePreviewLink: "",
+        image1Link: "",
+        image2Link: "",
+        authorName: "",
+        articlePreview: "",
+        articleContent: ""
+    }
+}
